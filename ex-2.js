63b2f6f33aff0385fb5e59e915feb4ce51b3dd40
Eisss20 <pajenan.j@gmail.com>
2024-10-03 15:01:43 +0700

    Feat-Anwser-assignment if-else

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,24 @@
 //Exercise 2
 let lightBulbStatus = "On";
 
-// Start coding here.
+if (lightBulbStatus === "On") {
+  console.log("Light bulb is On.");
+} else {
+  console.log("light bulb is off.");
+}
+
+console.log(lightBulbStatus);
+
+lightBulbStatus = "aaa";
+
+if (lightBulbStatus === "On") {
+  console("Light bulb is On.");
+} else if (lightBulbStatus === "of") {
+  console.log("Light bulb is Off.");
+} else if (lightBulbStatus === "Broken") {
+  console.log("Light bulb is Broken.");
+} else {
+  console.log("Please choose the correct input (On/Off)");
+}
+
+console.log(lightBulbStatus);
--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,12 @@
 //Exercise 3
 // Ternary Operator
 let lightBulbStatus = "Off";
+let massage;
 
-//Start coding here
+   massage = (lightBulbStatus === "On") 
+    ? "Light bulb is On."
+    : (lightBulbStatus === "Off")
+    ? "Light bulb is Off."
+    : "Please choose the correct input (On/Off/Broken)";
+
+console.log(massage);
